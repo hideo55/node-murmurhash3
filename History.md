fa4243f43380c365db7396490c247d2311b2811d
hideo55 <hide.o.j55@gmail.com>
2011-10-23 15:31:08 +0900

    Checking in changes prior to tagging of version 0.0.3.
    Changelog diff is:
    
    diff --git a/History.md b/History.md
    index 35a692b..45b21e2 100644
    --- a/History.md
    +++ b/History.md
    @@ -1,5 +1,11 @@
    +0.0.3 / 2011-10-23
    +==================
    +
    +  * Remove dependency
    +
     0.0.2 / 2011-09-25
     ==================
    +
       * Remove disused header including
       * Add repository to package.json

--- History.md
+++ History.md
@@ -1,5 +1,11 @@
+0.0.3 / 2011-10-23
+==================
+
+  * Remove dependency
+
 0.0.2 / 2011-09-25
 ==================
+
   * Remove disused header including
   * Add repository to package.json
 
--- test.js
+++ test.js
@@ -1,30 +1,23 @@
 var mmh3 = require('./lib/murmurhash3');
-var should = require('should');
+var assert = require('assert');
 
-var test_data = [
-		[ "Hello", 0x12da77c8, [ 0x2360ae46, 0x5e6336c6, 0xad45b3f4, 0xad45b3f4 ] ],
-		[ "Hello1", 0x6357e0a6, [ 0x8eb0cf41, 0x641b2401, 0xbc4c0dfc, 0xbc4c0dfc ] ],
-		[ "Hello2", 0xe5ce223e, [ 0xd3bcfc45, 0x66782162, 0x4beab2d1, 0x4beab2d1 ] ] ];
+var test_data = [["Hello", 0x12da77c8, [0x2360ae46, 0x5e6336c6, 0xad45b3f4, 0xad45b3f4]], ["Hello1", 0x6357e0a6, [0x8eb0cf41, 0x641b2401, 0xbc4c0dfc, 0xbc4c0dfc]], ["Hello2", 0xe5ce223e, [0xd3bcfc45, 0x66782162, 0x4beab2d1, 0x4beab2d1]]];
 
-for (i = 0; i < test_data.length; i++) {
-	(function() {
-		var val = test_data[i];
-		var res;
-		res = mmh3.murmur32(val[0]);
-		res.should.equal(val[1]);
-		
-		res = mmh3.murmur32Hex(val[0]);
-		res.should.equal(val[1].toString(16));
+for( i = 0; i < test_data.length; i++) {(function() {
+    var val = test_data[i];
+    var res;
+    res = mmh3.murmur32(val[0]);
+    assert.equal(res, val[1], 'murmur32-' + i);
+    res = mmh3.murmur32Hex(val[0]);
+    assert.equal(res, val[1].toString(16), 'murmur32Hex-' + i);
+    res = mmh3.murmur128(val[0]);
+    assert.deepEqual(res, val[2], 'murmur128-' + i);
+    res = mmh3.murmur128Hex(val[0]);
+    var expect = '';
+    for(var j = 0; j < 4; j++) {
+      expect += val[2][j].toString(16);
+    }
+    assert.equal(res, expect, 'murmur128Hex-' + i);
 
-		res = mmh3.murmur128(val[0]);
-		res.should.eql(val[2]);
-		
-		res = mmh3.murmur128Hex(val[0]);
-		var expect = '';
-		for( var j = 0 ; j< 4; j++){
-			expect += val[2][j].toString(16);
-		}
-		res.should.equal(expect);
-		
-	})();
+  })();
 }
