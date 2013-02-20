40c892fdf5622c7ecd5c3d3b43ee4aca376bec16
hideo55 <hide.o.j55@gmail.com>
2013-02-21 02:08:15 +0900

    Change test runner(nodeunit to whiskey)

--- test.js
+++ test.js
@@ -10,53 +10,50 @@ function zeroFill(number, width) {
   return number + ""; // always return a string
 }
 
-exports.setUp = function(cb) {
-  cb();
-};
 for(var i = 0; i < testData.length; i++) {
   var val = testData[i];
-  exports["testAsyncInterface32_" + i] = function(test) {
+  exports["testAsyncInterface32_" + i] = function(test, assert) {
     mmh3.murmur32(val[0], function(err, res) {
-      test.equal(res, val[1], 'murmur32-' + i);
-      test.done();
+      assert.equal(res, val[1], 'murmur32-' + i);
+      test.finish();
     });
   };
-  exports["testAsyncInterface32Hex_" + i] = function(test) {
+  exports["testAsyncInterface32Hex_" + i] = function(test, assert) {
     mmh3.murmur32Hex(val[0], function(err, res) {
-      test.equal(res, zeroFill(val[1].toString(16), 8), 'murmur32Hex-' + i);
-      test.done();
+      assert.equal(res, zeroFill(val[1].toString(16), 8), 'murmur32Hex-' + i);
+      test.finish();
     });
   };
-  exports["testAsyncInterface128_" + i] = function(test) {
+  exports["testAsyncInterface128_" + i] = function(test, assert) {
     mmh3.murmur128(val[0], function(err, res) {
-      test.deepEqual(res, val[2], 'murmur128-' + i);
-      test.done();
+      assert.deepEqual(res, val[2], 'murmur128-' + i);
+      test.finish();
     });
   };
-  exports["testAsyncInterface128Hex_" + i] = function(test) {
+  exports["testAsyncInterface128Hex_" + i] = function(test, assert) {
     mmh3.murmur128Hex(val[0], function(err, res) {
       var expect = '';
       for(var j = 0; j < 4; j++) {
         expect += zeroFill(val[2][j].toString(16), 8);
       }
-      test.equal(res, expect, 'murmur128Hex-' + i);
-      test.done();
+      assert.equal(res, expect, 'murmur128Hex-' + i);
+      test.finish();
     });
   };
-  exports["testSyncInterface_" + i] = function(test){
+  exports["testSyncInterface_" + i] = function(test, assert){
       var res;
       res = mmh3.murmur32Sync(val[0]);
-      test.equal(res, val[1], 'murmur32-' + i);
+      assert.equal(res, val[1], 'murmur32-' + i);
       res = mmh3.murmur32HexSync(val[0]);
-      test.equal(res, zeroFill(val[1].toString(16), 8), 'murmur32Hex-' + i);
+      assert.equal(res, zeroFill(val[1].toString(16), 8), 'murmur32Hex-' + i);
       res = mmh3.murmur128Sync(val[0]);
-      test.deepEqual(res, val[2], 'murmur128-' + i);
+      assert.deepEqual(res, val[2], 'murmur128-' + i);
       res = mmh3.murmur128HexSync(val[0]);
       var expect = '';
       for(var j = 0; j < 4; j++) {
         expect += zeroFill(val[2][j].toString(16), 8);
       }
-      test.equal(res, expect, 'murmur128Hex-' + i);
-      test.done();   
+      assert.equal(res, expect, 'murmur128Hex-' + i);
+      test.finish();
   };
 }
