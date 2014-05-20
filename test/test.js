f162b051821277837fb61ad877fd0c473785f10f
hideo55 <hide.o.j55@gmail.com>
2014-05-21 00:22:35 +0900

    Change test layout

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var mmh3 = require('./lib/murmurhash3');
+var mmh3 = require('../lib/murmurhash3');
 var testData = [["Hello", 0x12da77c8, [0x2360ae46, 0x5e6336c6, 0xad45b3f4, 0xad45b3f4]], ["Hello1", 0x6357e0a6, [0x8eb0cf41, 0x641b2401, 0xbc4c0dfc, 0xbc4c0dfc]], ["Hello2", 0xe5ce223e, [0xd3bcfc45, 0x66782162, 0x4beab2d1, 0x4beab2d1]], ["2", 0x0129e217, [0x9dd4f4e7, 0x3df769b3, 0x3df769b3, 0x3df769b3]], ["88", 0x7a0040a5, [0x28979fa9, 0x0b1b1a58, 0x0b1b1a58, 0x0b1b1a58]]];
 
 function zeroFill(number, width) {
